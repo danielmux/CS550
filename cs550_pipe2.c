ab9b46ac8dc25e917cd7afe74cd3bcdbd5a9bc0c
Daniel Iparraguirre <daniel.iparraguirre@wsu.edu>
2013-04-06 18:42:04 -0700

    Trying pipe.c functions

--- cs550_pipe2.c
+++ cs550_pipe2.c
@@ -8,10 +8,10 @@ main()
    pipe(pd);     // create a pipe
    if (fork()){  // fork a child as WRITER, parent as READER
        printf("parent  %d close pd[1]\n", getpid());
-       close(pd[1]);
+       close_pipe(pd[1]);
        while(i++ < 10){  // parent read from pipe
            printf("parent  %d reading from pipe\n", getpid());
-           if ((n = read(pd[0], line, 256)) == 0)
+           if ((n = read_pipe(pd[0], line, 256)) == 0)
                exit(0);
            line[n]=0; printf("%s  n=%d\n",line,  n);
        }
@@ -20,10 +20,10 @@ main()
    }
    else{         // child as pipe WRITER
        printf("child %d close pd[0]\n", getpid());
-       close(pd[0]);
+       close_pipe(pd[0]);
        while(1){   // child writes to pipe
            printf("child %d writing pipe : %s\n", getpid(), s);
-           write(pd[1], s, strlen(s));
+           write_pipe(pd[1], s, strlen(s));
        }
    }
 }

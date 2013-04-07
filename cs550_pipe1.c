78658523c9fcd4450a241a95f8d4caa4f331f6ac
Daniel Iparraguirre <daniel.iparraguirre@wsu.edu>
2013-04-06 18:01:01 -0700

    w/ pipe2

--- cs550_pipe1.c
+++ cs550_pipe1.c
@@ -1,4 +1,4 @@
-#include <uinclude.h>
+#include "uinclude.h"
 
 int  pd[2], n, i;
 char line[256], *s="data from pipe ";
@@ -6,24 +6,24 @@ char line[256], *s="data from pipe ";
 main()
 {
    pipe(pd);     // create a pipe
-   if (fork()){  // fork a child as WRITER, parent as READER
-       printf("parent  %d close pd[1]\n", getpid());
-       close(pd[1]);
-       while(i++ < 10){  // parent read from pipe
-           printf("parent  %d reading from pipe\n", getpid());
-           if ((n = read(pd[0], line, 256)) == 0)
-               exit(0);
-           line[n]=0; printf("%s  n=%d\n",line,  n);
+   if (fork()){  // fork a child as READER, parent as WRITER
+       printf("parent  %d close pd[0]\n", getpid());
+       close(pd[0]);
+       while(i++ < 10){  // parent writes to pipe
+           printf("parent  %d writing pipe: %s\n", getpid(), s);
+           write(pd[1], s, strlen(s));
        }
        printf("parent %d exit\n", getpid());
        exit(0);
    }
-   else{         // child as pipe WRITER
-       printf("child %d close pd[0]\n", getpid());
-       close(pd[0]);
-       while(1){   // child writes to pipe
-           printf("child %d writing pipe : %s\n", getpid(), s);
-           write(pd[1], s, strlen(s));
+   else{         // child as pipe READER
+       printf("child %d close pd[1]\n", getpid());
+       close(pd[1]);
+       while(1){   // child reads from pipe
+           printf("child %d reading from pipe\n", getpid());
+           if ((n = read(pd[0], line, 256)) == 0)
+               exit(0);
+           line[n]=0; printf("%s  n=%d\n",line,  n);
        }
    }
 }
